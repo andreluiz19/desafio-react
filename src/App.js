54165d55b98e558fbdd392593ab6d34bc3462954
andreluiz19 <andreavancini@alunos.utfpr.edu.br>
2022-12-26 10:40:59 -0300

    desafio

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [list, setList] = useState([]);
+  const [undid, setUndid] = useState([]);
+
+  const handleClick = (event) => {
+    const newDot = {
+      clientX: event.clientX,
+      clientY: event.clientY
+    }
+    console.log(newDot);
+    // Cria um novo array com o spread operator e adiciona o novo objeto
+    setList((prev) => [...prev, newDot]);
+    setUndid([]);
+  };
+
+  const handleUndo = (event) => {
+    // Não deixa que o evento do elemento pai atinja o elemento filho
+    event.stopPropagation();
+    console.log("undo");
+
+    if (list.length === 0) {
+      return;
+    }
+
+    const lastItem = list[list.length - 1];
+    setUndid((prev) => [...prev, lastItem]);
+
+    setList((prev) => {
+      const newArr = [...prev].slice(0, -1);
+      return newArr;
+    });
+  };
+
+  const handleRedo = (event) => {
+    event.stopPropagation();
+    console.log("redo");
+
+    if (undid.length === 0) {
+      return;
+    }
+
+    const recoveredItem = undid[undid.length - 1];
+    setUndid((prev) => {
+      const newArr = [...prev].slice(0, -1);
+      return newArr;
+    });
+    setList((prev) => [...prev, recoveredItem]);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" id="page" onClick={handleClick}>
+      <button className="btn" onClick={handleUndo}>Desfazer</button>
+      <button className="btn" onClick={handleRedo}>Refazer</button>
+      {list.map((item, index) => (
+        <span 
+          key={index}
+          className="dot"
+          style={{ left: item.clientX, top: item.clientY }}
+        />
+      ))}
+
+
     </div>
   );
 }
